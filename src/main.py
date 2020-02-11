21337e79b22bacdf9de12d5dc922e51d56b97cd4
Takashi Kodama <kodama@nlp.ist.i.kyoto-u.ac.jp>
2020-02-11 19:49:01 +0900

    fix main.py

--- src/main.py
+++ src/main.py
@@ -5,21 +5,20 @@ from joblib import Parallel, delayed
 
 from clean_text import clean_text
 
+DELIMITER = {'txt': None, 'csv': ',', 'tsv': '\t'}
+JOINER = {'txt': '', 'csv': ',', 'tsv': '\t'}
 
-def _clean_texts(input_text, delimiter, twitter):
-    if input_text:
-        if delimiter:
-            print(delimiter.join([clean_text(text=text, twitter=twitter) for text in input_text.split(delimiter)]))
-        else:
-            print(clean_text(text=input_text, twitter=twitter))
-    else:
-        print()
+
+def _clean_texts(input_text, file_format, twitter):
+    delimiter = DELIMITER[file_format]
+    joiner = JOINER[file_format]
+    print(joiner.join([clean_text(text=text, twitter=twitter) for text in input_text.split(delimiter)]))
 
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('-d', '--delimiter', default='', type=str)
-    parser.add_argument('-n', '--n_jobs', default=10, type=int)
+    parser.add_argument('-f', '--file-format', default='txt', type=str, choices=['txt', 'csv', 'tsv'])
+    parser.add_argument('-n', '--n-jobs', default=10, type=int)
     parser.add_argument('-t', '--twitter', action='store_true')
     parser.add_argument('-i', '--input-file', type=str)
     args = parser.parse_args()
@@ -30,7 +29,7 @@ def main():
     else:
         input_texts = ''.join(sys.stdin.readlines())
 
-    Parallel(n_jobs=args.n_jobs, verbose=10)([delayed(_clean_texts)(input_text, args.delimiter, args.twitter)
+    Parallel(n_jobs=args.n_jobs, verbose=10)([delayed(_clean_texts)(input_text, args.file_format, args.twitter)
                                               for input_text in input_texts.split('\n')])
 
 
