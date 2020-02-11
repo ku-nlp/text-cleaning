7fdfa66f6ff8cf0109a1b99ddfdd124169923422
nobu-g <ueda-n127@hotmail.co.jp>
2020-02-11 21:03:22 +0900

    deal with  UnicodeDecodeError

--- src/main.py
+++ src/main.py
@@ -23,8 +23,14 @@ def main():
     parser.add_argument('-i', '--input-file', type=str)
     args = parser.parse_args()
 
-    with open(args.input_file, 'r') if args.input_file else sys.stdin as f:
-        input_texts = [line.strip() for line in f.readlines()]
+    input_texts = []
+    with open(args.input_file, 'rb') if args.input_file else sys.stdin as f:
+        for line in f.buffer:
+            try:
+                line = line.decode('utf-8')
+            except UnicodeDecodeError:
+                line = ''
+        input_texts.append(line.strip())
 
     outputs = Parallel(n_jobs=args.n_jobs, verbose=10)(
         [delayed(_clean_texts)(input_text, args.file_format, args.twitter) for input_text in input_texts])
